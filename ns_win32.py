073d1d33f6ffc4d3342104ab0775efa9bd96dc2b
Namsic <kimnj3050@gmail.com>
2020-04-01 11:32:25 +0900

    Reduce delay
    Remove pyautogui and replace win32api

--- ns_win32.py
+++ ns_win32.py
@@ -1,4 +1,4 @@
-import win32api, win32gui, win32con
+import win32api, win32gui
 import time
 import subprocess
 
@@ -8,6 +8,14 @@ MOUSE_LEFTUP = 0x0004
 MOUSE_RIGHTDOWN = 0x0008
 MOUSE_RIGHTUP = 0x0010
 MOUSE_WHEEL = 0x0800
+
+
+def mouse_move(x, y, rel=False):
+    x1, y1 = win32api.GetCursorPos()
+    if rel:
+        win32api.SetCursorPos((x1+x, y1+y))
+    else:
+        win32api.SetCursorPos((x, y))
 
 
 def mouse_click(x, y):
@@ -22,9 +30,17 @@ def keyboard_press(k):
     win32api.keybd_event(k, 0, 0x00, 0)  # KeyDown
     win32api.keybd_event(k, 0, 0x02, 0)  # KeyUp
 
+
+def window_set(w_title, position=None, size=None):
+    hwnd = win32gui.FindWindow(None, w_title)
+    info = win32gui.GetWindowRect(hwnd)
+    if position == None:
+        position = (info[0], info[1])
+    if size == None:
+        size = (info[2]-info[0], info[3]-info[1])
+    win32gui.MoveWindow(hwnd, position[0], position[1], size[0], size[1], True)
+    return position
+
+
 if __name__ == '__main__':
-    time.sleep(3)
-    print('s')
-    #key_press(0x25)
-    key_press(0x0D)
-    print('e')
+    osk_init()
